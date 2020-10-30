3024dff3233a931381415bf0ee618975776a2340
Christopher Raible <cmraible1@gmail.com>
2020-10-30 16:14:47 -0700

    adding stroke color to svg

--- components/DiagonalSection.js
+++ components/DiagonalSection.js
@@ -11,7 +11,7 @@ const DiagonalSection = ({ background, color, children }) => {
   return (
     <Box fill>
       <svg height="100px" width="100%" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
-        <polygon fill={background} points="0,100 100,0 100,100"/>
+        <polygon stroke={background} fill={background} points="0,100 100,0 100,100"/>
       </svg>
       <Box pad="xlarge" background={background} color={color}>
         {children}

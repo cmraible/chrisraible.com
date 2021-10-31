cb490ff4f723459eb08610a679d3c306286c11d4
Chris Raible <cmraible1@gmail.com>
2021-10-30 18:35:22 -0700

    adding first post!

--- components/SiteFooter.js
+++ components/SiteFooter.js
@@ -7,8 +7,8 @@ const SiteFooter = () => {
   const router = useRouter();
 
   return (
-    <Box fill="horizontal">
-      <Footer fill="horizontal" background="#CCCCCC" style={{width: '100%', zIndex: 1}} pad="medium" >
+    <Box fill="horizontal" height="10vh">
+      <Footer fill background="#CCCCCC" style={{width: '100%', zIndex: 1}} pad="medium" >
         <Box justify="between" fill="horizontal" direction="row">
           <Box onClick={() => router.push('/')}>
             <Heading size="small" margin="none">Chris Raible</Heading>

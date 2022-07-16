4dc3185db8785593263462d85029fdec64756f71
Chris Raible <cmraible1@gmail.com>
2022-07-16 13:35:54 -0700

    adding youtube and tiktok links

--- components/SocialLinks.js
+++ components/SocialLinks.js
@@ -1,5 +1,5 @@
 import { Box, Button } from 'grommet';
-import { Github, Linkedin, Twitter } from 'grommet-icons';
+import { Github, Linkedin, Twitter, Youtube, Tictok } from 'grommet-icons';
 import { useRouter } from 'next/router';
 import React from 'react';
 
@@ -13,6 +13,8 @@ const SocialLinks = ( { size }) => {
         <Button icon={<Twitter color="gray" size={size} />} onClick={() => router.push('https://twitter.com/RaibleChris')}/>
         <Button icon={<Linkedin color="gray" size={size} />} onClick={() => router.push('https://www.linkedin.com/in/chrisraible/')}/>
         <Button icon={<Github color="gray" size={size} />} onClick={() => router.push('https://github.com/cmraible')}/>
+        <Button icon={<Youtube color="gray" size={size} />} onClick={() => router.push('https://www.youtube.com/channel/UCoVzT1bE9w5wklD2V15mqmw')} />
+        <Button icon={<Tictok color="gray" size={size} />} onClick={() => router.push('https://www.tiktok.com/@chrisraible')} />
     </Box>
   )
 }
